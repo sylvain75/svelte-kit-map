e647779e140cdb8055cc933e9becffb40bb2307a
Sylvain <sylvoz75@gmail.com>
2021-08-11 19:18:16 +1000

    first commit

---
+++ src/components/SharedCount.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let count: number;
+</script>
+
+<div>
+  shared comp with {count}
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let localCount = 0;
+  let textValue = '';
+  import SharedCount from '../components/SharedCount.svelte'
+  const handleChange = (operation: 'add' | 'substract') => {
+    localCount = operation === 'add' ? ++localCount : --localCount;
+  }
+
+  $: console.log('TEXT', textValue)
+  $: console.log('COUNT', localCount)
+</script>
+
+<h1>Welcome Count</h1>
+<button on:click={() => handleChange('substract')}>-</button>
+<button on:click={() => handleChange('add')}>+</button>
+<input type="text" bind:value={textValue} />
+<div>-------</div>
+<SharedCount count={localCount} />
